5b0c0fca787cb1821d994b89e979f2d6299e0d75
RyanSoup <rmc2@g.clemson.edu>
2023-12-19 14:10:18 -0500

    IDeck contract and start

--- Game/ControllerandModel/models/IDeck.java
+++ Game/ControllerandModel/models/IDeck.java
@@ -1,10 +1,28 @@
 package Game.ControllerandModel.models; 
-
+/**
+ * Deck represents a Deck of card objects
+ * 
+ * Initialization Ensures:
+ *                  Deck contains number of card objects equal to MAXCARDS
+ *                  No cards in the deck equal each other
+ * 
+ * Defines:
+ *        numOfCards: X
+ * 
+ * Constraints:
+ *          0 < numOfCards <= MAXCARDS
+ * 
+ * 
+ */
 
 
 
 public interface IDeck {
 
+    public final int MAXCARDS = 52; 
     
+
+    public void shuffleDeck(); 
+
     
 }
